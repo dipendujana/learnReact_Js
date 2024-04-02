57f82754fd6fa54c73cf284f96f4aba4924ef132
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-04-02 20:08:48 +0000

    update

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "Dipendu",
+    age: 21
+  }
 
   return (
     <>
-         <h1 className="text-3xl font-bold underline">
-           Hello world!
-         </h1>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind CSS</h1>
+
+    <Card username="Dipendu" banText="Click Koro"/>
+    <Card username="Hritash sir"/>
+
     </>
   )
 }
