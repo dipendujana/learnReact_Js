294a6bbba8ab8ef51211d5178f9ead5aa2b0dcdf
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-04-06 07:40:30 +0000

    update

--- 09themeswither/src/components/Card.jsx
+++ 09themeswither/src/components/Card.jsx
@@ -63,7 +63,7 @@ export default function Card() {
                     </span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$99</span>
                     <a
                         href="/"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
