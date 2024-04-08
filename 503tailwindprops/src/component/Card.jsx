ac767bffd6cb5c10f955b3a10875b50063c8001a
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-04-08 18:57:42 +0000

    test

--- 503tailwindprops/src/component/Card.jsx
+++ 503tailwindprops/src/component/Card.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 
-function Card({username,btnText="vist me"}) {
+function Card({ btnText="Bye Now", username="About Macbook"}) {
   return (
-    <div className="relative h-[400px] w-[300px] rounded-xl border-4 border-pink-600 " >
+    <div className="w-[300px] rounded-md border-4 bg-red-600">
     <img
-      src="https://images.pexels.com/photos/2138922/pexels-photo-2138922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      alt="AirMax Pro"
-      className="z-0 h-full w-full rounded-md object-cover"
+      src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
+      alt="Laptop"
+      className="h-[200px] w-full rounded-md object-cover"
     />
-    <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-    <div className="absolute bottom-4 left-4 text-left">
-      <h1 className="text-lg font-semibold text-white">{username}</h1>
-      <p className="mt-2 text-sm text-gray-300">
+    <div className="p-4">
+      <h1 className="text-lg font-semibold">{username}</h1>
+      <p className="mt-3 text-sm text-white">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
       </p>
-      <button className="mt-2 inline-flex bg-orange-500 p-5 rounded-lg cursor-pointer items-center text-sm font-semibold text-white">
-        {btnText} &rarr;
+      <button
+        type="button"
+        className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+      >
+        {btnText}
       </button>
     </div>
   </div>
-
   )
 }
 
