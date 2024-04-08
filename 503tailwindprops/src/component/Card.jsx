9b1d38a80aa1d508421da6930aa91c4adaa73776
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-04-08 19:00:03 +0000

    update

--- 503tailwindprops/src/component/Card.jsx
+++ 503tailwindprops/src/component/Card.jsx
@@ -15,7 +15,7 @@ function Card({ btnText="Bye Now", username="About Macbook"}) {
       </p>
       <button
         type="button"
-        className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+        className=" border-2 mt-4 rounded-xl bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
       >
         {btnText}
       </button>
