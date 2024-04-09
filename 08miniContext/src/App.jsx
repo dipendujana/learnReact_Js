2dfd0acda919c30b79c133d80236ddbca2aa33d2
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-04-09 09:59:36 +0530

    Update App.jsx

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <UserContextProvider>
       <h1>React with Chai and share is important</h1>
+<p>Mobile</>
       <Login />
       <Profile />
     </UserContextProvider>
