0b7d39ad03ebc0a970b5afcd2c959dbf989c1ac9
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-04-11 14:55:49 +0000

    update

--- 07ractRouter/src/App.jsx
+++ 07ractRouter/src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+// import Home from './components/Home/Home'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-         <h1 className="text-3xl font-bold underline text-red-700">
-      Hello world!
-    </h1>
-    
-        
+    <Home/>
+     
+      
     </>
   )
 }
