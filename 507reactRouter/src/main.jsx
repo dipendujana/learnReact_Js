7d012e34601ed061c97a79347a354d725dd3964d
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-04-12 15:25:14 +0000

    update react router

---
+++ 507reactRouter/src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import './index.css'
+import Layout from './Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      // {
+      //   path: "about",
+      //   element: <About />
+      // },
+      // {
+      //   path: "contact",
+      //   element: <Contact />
+      // }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
