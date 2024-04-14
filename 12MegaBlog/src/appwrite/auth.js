78b0ef9d5e5f0b8dc1b6b0a999bbd09391e2e57c
dipendujana <120547835+dipendujana@users.noreply.github.com>
2024-04-14 18:02:37 +0000

    mega project

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,15 @@
+import { Client, Account, ID } from "appwrite";
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+    .setProject('<PROJECT_ID>');               // Your project ID
+
+const account = new Account(client);
+
+const promise = account.create('[USER_ID]', 'email@example.com', '');
+
+promise.then(function (response) {
+    console.log(response); // Success
+}, function (error) {
+    console.log(error); // Failure
+});
